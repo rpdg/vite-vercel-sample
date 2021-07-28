48bed3889ff0e8b94083ea091cec0cbb14118135
Lyu Pengfei <pf_lyu@qq.com>
2021-07-28 17:27:49 +0800

    add help

--- src/components/MyItb/Help/index.tsx
+++ src/components/MyItb/Help/index.tsx
@@ -6,28 +6,62 @@ import styles from '../index.module.scss';
 export default defineComponent({
 	name: 'HelpSection',
 	setup() {
-		const {t} = useI18n();
+		const { t } = useI18n();
 
-		let bg = "//via.placeholder.com/100x100.jpg/230E12/230E12"
+		let bg = '//via.placeholder.com/100x100.jpg/230E12/230E12';
 
 		return () => (
-			<Card
-				title={t('myItb.dockIcons.help')}
-				style={{marginTop: '28px' }}
-			>
+			<Card title={t('myItb.dockIcons.help')} style={{ marginTop: '28px' }}>
 				<div class={styles.gridCols2}>
-					
-					<a href="//www.163.com" target="_blank" class={styles.helpItem} style={{backgroundImage: `url(${bg})`}}>
+					<a
+						href='//www.163.com'
+						target='_blank'
+						class={styles.helpItem}
+						style={{ backgroundImage: `url(${bg})` }}
+					>
 						<h2>{t('myItb.help.contact_sponsor')}</h2>
 						<p>{t('myItb.help.enter')}</p>
 					</a>
 
-					<a href="//www.163.com" target="_blank" class={styles.helpItem} style={{backgroundImage: `url(${bg})`}}>
+					<a
+						href='//www.163.com'
+						target='_blank'
+						class={styles.helpItem}
+						style={{ backgroundImage: `url(${bg})` }}
+					>
 						<h2>{t('myItb.help.want_settle')}</h2>
 						<p>{t('myItb.help.enter')}</p>
 					</a>
-					
-					
+
+					<a
+						href='//www.163.com'
+						target='_blank'
+						class={styles.helpItem}
+						style={{ backgroundImage: `url(${bg})` }}
+					>
+						<h2>{t('myItb.help.shuttle_bus')}</h2>
+						<p>{t('myItb.help.enter')}</p>
+					</a>
+
+					<a
+						href='//www.163.com'
+						target='_blank'
+						class={styles.helpItem}
+						style={{ backgroundImage: `url(${bg})` }}
+					>
+						<h2>{t('myItb.help.hotel_services')}</h2>
+						<p>{t('myItb.help.enter')}</p>
+					</a>
+
+					<a
+						href='//www.163.com'
+						target='_blank'
+						class={styles.helpItem}
+						style={{ backgroundImage: `url(${bg})` }}
+					>
+						<h2>{t('myItb.help.support')}</h2>
+						<p>{t('myItb.help.enter')}</p>
+					</a>
 				</div>
 			</Card>
 		);
