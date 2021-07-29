19e34fb1af04a7c23b76232baa6a7f2ea69170e4
Lyu Pengfei <pf_lyu@qq.com>
2021-07-29 16:15:54 +0800

    nightly build

--- static/assets/403.ce4725f6.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.9dfa3a0e.js";var a=Vue.defineComponent({name:"Page403",setup:()=>()=>Vue.createVNode(e,{code:403,message:"Not permitted"},null)});export default a;
--- static/assets/404.59f4bdae.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.9dfa3a0e.js";var a=Vue.defineComponent({name:"Page403",setup:()=>()=>Vue.createVNode(e,{code:404,message:"Page not found"},null)});export default a;
