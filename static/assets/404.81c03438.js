1c1fee8afbbe5c5842f7fc96ed9a5a013dff891b
Lyu Pengfei <lyu1975@gmail.com>
2021-07-30 02:52:53 +0000

    nightly build

---
+++ static/assets/404.81c03438.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.95beede7.js";var a=Vue.defineComponent({name:"Page403",setup:()=>()=>Vue.createVNode(e,{code:404,message:"Page not found"},null)});export{a as default};
--- static/assets/404.e8f8c8c9.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.552ba0cf.js";var a=Vue.defineComponent({name:"Page403",setup:()=>()=>Vue.createVNode(e,{code:404,message:"Page not found"},null)});export{a as default};
