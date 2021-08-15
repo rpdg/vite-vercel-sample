6ac2d69198f5d85263578b678a7977fda4d0cf9d
Lyu Pengfei <lyu1975@gmail.com>
2021-08-15 21:55:49 +0800

    nightly build (2021-08-15T21:54:35)

---
+++ static/assets/403.f3410de7.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.3851a5e7.js";import{a5 as a}from"./@vue.01a79c59.js";const o=()=>a(e,{code:403,message:"Not permitted"},null);export{o as default};
---
+++ static/assets/404.80f173ee.js
@@ -0,0 +1,1 @@
+import{E as a}from"./index.3851a5e7.js";import{a5 as e}from"./@vue.01a79c59.js";const o=()=>e(a,{code:404,message:"Page not found"},null);export{o as default};
