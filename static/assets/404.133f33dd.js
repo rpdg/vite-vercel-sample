56607edd96fe335b4e6e306a83cf14c28c0774a9
Lyu Pengfei <lyu1975@gmail.com>
2021-09-02 09:55:49 +0800

    nightly build (2021-09-02T09:54:23)

---
+++ static/assets/404.133f33dd.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.462b28e2.js";import{a0 as a}from"./@vue.40aa8880.js";export default()=>a(e,{code:404,message:"Page not found"},null);
---
+++ static/assets/index.462b28e2.js
@@ -0,0 +1,1 @@
+import{a2 as e,a0 as r}from"./@vue.40aa8880.js";var a="_errorContainer_12ed2_1",s="_buildInfo_12ed2_17",o=e({props:{code:{type:Number,reuuired:!0},message:{type:String,required:!0}},name:"ErrorLayout",setup:(e,{slots:o})=>()=>r("div",{class:a},[r("h1",null,[e.code]),r("span",null,[e.message]),r("p",{class:s},["Updated 2021/09/02 09:55:09"])])});export{o as E};
