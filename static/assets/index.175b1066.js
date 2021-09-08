074c06d26bdca9310f16fc732d4bd05891a5066b
Lyu Pengfei <lyu1975@gmail.com>
2021-09-08 10:12:04 +0800

    nightly build (2021-09-08T10:10:23)

---
+++ static/assets/index.175b1066.js
@@ -0,0 +1,1 @@
+import{a2 as e,a0 as r}from"./@vue.5723de44.js";var s="_errorContainer_12ed2_1",a="_buildInfo_12ed2_17",o=e({props:{code:{type:Number,reuuired:!0},message:{type:String,required:!0}},name:"ErrorLayout",setup:(e,{slots:o})=>()=>r("div",{class:s},[r("h1",null,[e.code]),r("span",null,[e.message]),r("p",{class:a},["Updated 2021/09/08 10:11:21"])])});export{o as E};
