aae3dec7f66d2446c8c49613c00aa414e35c077a
Lyu Pengfei <lyu1975@gmail.com>
2021-09-18 10:05:17 +0800

    nightly build (2021-09-18T10:03:54)

--- static/assets/404.3763a2f7.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.488a9805.js";import{a0 as o}from"./@vue.5723de44.js";export default()=>o(e,{code:404,message:"Page not found"},null);
---
+++ static/assets/404.6cf97e16.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.ef5f43bc.js";import{a0 as o}from"./@vue.5723de44.js";export default()=>o(e,{code:404,message:"Page not found"},null);
