cb447cf4389e0d2b42e6ba771fcc7ffce81aa392
Lyu Pengfei <lyu1975@gmail.com>
2021-10-13 15:01:15 +0800

    nightly build (2021-10-13T14:59:43)

--- static/assets/404.4167add1.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.a1eca83d.js";import{k as o}from"./@vue.f0f90a02.js";import"./vue-i18n.8989ea2a.js";import"./@intlify.35d5f340.js";import"./source-map.3671d69c.js";import"./vue.8bc7d658.js";const d=()=>o(e,{code:404,message:"Page not found"},null);export{d as default};
---
+++ static/assets/index.module.efae9bd3.js
@@ -0,0 +1,1 @@
+const a="_item_kdpzw_1",_="_avatar_kdpzw_6",t="_profile_kdpzw_10",r="_iconBar_kdpzw_14",i="_avatar_special_kdpzw_31";var o={item:a,avatar:_,profile:t,iconBar:r,avatar_special:i};export{o as s};
