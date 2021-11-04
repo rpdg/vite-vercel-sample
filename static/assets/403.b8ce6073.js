de8c2380daa07dde9edb7c871470883780cdba6b
Lyu Pengfei <pf_lyu@qq.com>
2021-11-04 10:35:02 +0800

    clean

--- static/assets/403.b8ce6073.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.4c22ee8c.js";import{k as o}from"./@vue.6f7c75f1.js";import"./nprogress.35503c85.js";const a=()=>o(e,{code:403,message:"Not permitted"},null);export{a as default};
