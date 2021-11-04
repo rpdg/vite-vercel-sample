97187c3f5b7939f8602ec5345b5990cf6f4b85b6
Lyu Pengfei <lyu1975@gmail.com>
2021-11-04 10:58:03 +0800

    Removed previouse dist directory

--- static/assets/Image.174dcbe6.js
+++
@@ -1,1 +0,0 @@
-import{B as f}from"./BlankGif.8ca146ab.js";import{S as n}from"./Config.41849c7b.js";function r(e="",i=f){let t;if(e)e.indexOf("http://")===0||e.indexOf("https://")===0||e.indexOf("//")===0?t=e:t=`${n}${e.replace(/\\/g,"/")}`;else return i;return t}export{r as p};
