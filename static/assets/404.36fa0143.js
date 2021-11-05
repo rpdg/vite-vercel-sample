dfc8f20aeef24aa06977047f4dd722fd447b8d51
Lyu Pengfei <lyu1975@gmail.com>
2021-11-05 18:47:34 +0800

    Remove previouse dist directory

--- static/assets/404.36fa0143.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.9d40cb8e.js";import{k as o}from"./@vue.f0a33739.js";import"./nprogress.35503c85.js";const t=()=>o(e,{code:404,message:"Page not found"},null);export{t as default};
