22ef91fef128fcaafa4685fd5e19042c972bd5c5
Lyu Pengfei <lyu1975@gmail.com>
2021-11-05 12:28:12 +0800

    Remove previouse dist directory

--- static/assets/403.fbc25b87.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.7f5ce441.js";import{k as o}from"./@vue.f0a33739.js";import"./nprogress.35503c85.js";const a=()=>o(e,{code:403,message:"Not permitted"},null);export{a as default};
