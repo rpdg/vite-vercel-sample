679417db902153375035e6a6921bcd3f1e672a8a
Lyu Pengfei <lyu1975@gmail.com>
2021-11-07 17:32:46 +0800

    nightly build (2021-11-07T17:31:24)

---
+++ static/assets/404.2c0181d7.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.11989b7e.js";import{k as o}from"./@vue.f0a33739.js";import"./nprogress.35503c85.js";const t=()=>o(e,{code:404,message:"Page not found"},null);export{t as default};
