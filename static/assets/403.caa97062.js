432a0dd5de41e0a1d822a1d3db42c7ee69f0202d
rpdg <lyu1975@gmail.com>
2021-11-16 02:25:30 +0000

    Deploy https://github.com/rpdg/itb-plus-console/commit/57437c91437c03dbb73f469880822e9aa96aca5e

---
+++ static/assets/403.caa97062.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.a63da3a2.js";import{k as o}from"./@vue.f0a33739.js";import"./nprogress.35503c85.js";const t=()=>o(e,{code:403,message:"Not permitted"},null);export{t as default};
---
+++ static/assets/404.b4b21855.js
@@ -0,0 +1,1 @@
+import{E as o}from"./index.b8305bda.js";import{k as e}from"./@vue.f0a33739.js";import"./nprogress.35503c85.js";const t=()=>e(o,{code:404,message:"Page not found"},null);export{t as default};
