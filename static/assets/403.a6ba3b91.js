af557ed7ef9510bd6caf27e8a011a27df5d11e1a
Lyu Pengfei <lyu1975@gmail.com>
2022-09-01 17:27:05 +0800

    nightly build (2022-09-01T17:25:40)

---
+++ static/assets/403.a6ba3b91.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.2cce70d1.js";import{k as o}from"./@vue.c37eb56a.js";import"./nprogress.35503c85.js";const a=()=>o(e,{code:403,message:"Not permitted"},null);export{a as default};
---
+++ static/assets/404.e0a65951.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.2cce70d1.js";import{k as o}from"./@vue.c37eb56a.js";import"./nprogress.35503c85.js";const t=()=>o(e,{code:404,message:"Page not found"},null);export{t as default};
