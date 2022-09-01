b45f6b212fd7cb67e196430a68fecde39997df94
Lyu Pengfei <lyu1975@gmail.com>
2022-09-01 17:25:42 +0800

    Remove previouse dist directory

--- static/assets/404.54752bb9.js
+++
@@ -1,1 +0,0 @@
-import{E as o}from"./index.b077d452.js";import{k as e}from"./@vue.f0a33739.js";import"./nprogress.35503c85.js";const t=()=>e(o,{code:404,message:"Page not found"},null);export{t as default};
--- static/assets/avator_red.2a3a1020.js
+++
@@ -1,1 +0,0 @@
-var a="/assets/avator_red.e64328ae.png";export{a as d};
