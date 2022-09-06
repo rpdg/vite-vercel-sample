6a1a8c46c58d321c716b42c4bfcb6aa25c32a297
Lyu Pengfei <lyu1975@gmail.com>
2022-09-06 14:45:12 +0800

    Remove previouse dist directory

--- static/assets/404.30b9fb64.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.5bed6319.js";import{k as o}from"./@vue.c37eb56a.js";import"./nprogress.35503c85.js";const t=()=>o(e,{code:404,message:"Page not found"},null);export{t as default};
