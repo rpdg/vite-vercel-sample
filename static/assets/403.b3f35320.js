488480d1e670482de117f5613427325e575f025b
Lyu Pengfei <lyu1975@gmail.com>
2023-06-14 15:49:54 +0800

    nightly build (2023-06-14T15:48:39)

---
+++ static/assets/403.b3f35320.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.94f6ec7c.js";import{k as o}from"./@vue.92a87908.js";import"./nprogress.35503c85.js";const a=()=>o(e,{code:403,message:"Not permitted"},null);export{a as default};
